e24898ac0836588e4ed1cc6478084df94dd50e65
jyj <2534803898@qq.com>
2024-03-19 18:38:36 +0800

    ..

--- TestAvRegion/WpfApp1/MainWindow.xaml.cs
+++ TestAvRegion/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Prism.Regions;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,12 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(IRegionManager regionManager)
         {
             InitializeComponent();
+
+            RegionManager.SetRegionManager(this, regionManager);
+            RegionManager.UpdateRegions();
         }
     }
 }

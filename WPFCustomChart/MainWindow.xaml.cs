d6b66218adcda1d5ca6605396b63ac97250c0893
jyj <2534803898@qq.com>
2024-04-08 17:44:41 +0800

    1.自定义图表测试 2.自动创建dump

---
+++ WPFCustomChart/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCustomChart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public VisualHost1 _visualHost;
+        double scale;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _visualHost = new VisualHost1();
+            DrawingContainer.Children.Add(_visualHost);
+
+            double canvasWidth = DrawingContainer.Width;
+            double canvasHeight = DrawingContainer.Height;
+            //double canvasWidth = 3000;
+            //double canvasHeight = 3000;
+
+
+            //new SelectBoxRect(DrawingContainer);
+
+            //using (new PerformanceCounter("ghfgh"))
+            using (new PerformanceCounter())
+            {
+                DrawRectangles1(canvasWidth, canvasHeight);
+            }
+            scale = 1;
+        }
+
+        private void Window_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                if (e.Delta < 0)
+                {
+                    scale -= 0.01;
+                }
+                else
+                {
+                    scale += 0.01;
+                }
+                // scale += (double)e.Delta / 35000;
+                ScaleTransform transfrom = new ScaleTransform();
+                transfrom.ScaleX = transfrom.ScaleY = scale;
+                this.DrawingContainer.RenderTransform = transfrom;
+            }
+        }
+
+        private Pen CreateAndFreezePen()
+        {
+            // 创建Pen
+            Pen pen = new Pen(Brushes.Black, 1);
+
+            // 冻结Pen
+            if (pen.CanFreeze)
+            {
+                pen.Freeze();
+            }
+
+            return pen;
+        }
+
+        private void DrawRectangles1(double canvasWidth, double canvasHeight)
+        {
+            int rows = 100; // 纵向矩形数量
+            int columns = 100; // 横向矩形数量
+
+
+            using (DrawingContext drawingContext = _visualHost.RenderOpen())
+            {
+
+                double rectangleWidth = canvasWidth / columns;
+                double rectangleHeight = canvasHeight / rows;
+
+                var pen = CreateAndFreezePen();
+                for (int i = 0; i < columns; i++)
+                {
+                    for (int j = 0; j < rows; j++)
+                    {
+                        double x = i * rectangleWidth;
+                        double y = j * rectangleHeight;
+
+                        // 创建矩形几何图形
+                        Rect rectangleRect = new Rect(new Point(x, y), new Size(rectangleWidth, rectangleHeight));
+                        Geometry rectangleGeometry = new RectangleGeometry(rectangleRect);
+
+                        // 绘制矩形
+                        //drawingContext.DrawGeometry(Brushes.Blue, new Pen(Brushes.Black, 1), rectangleGeometry);
+                        drawingContext.DrawGeometry(Brushes.Blue, pen, rectangleGeometry);
+
+                    }
+                }
+            }
+        }
+    }
+
+    public class VisualHost1 : FrameworkElement
+    {
+        private readonly DrawingVisual _drawingVisual;
+
+        public VisualHost1()
+        {
+            _drawingVisual = new DrawingVisual();
+        }
+
+        public DrawingContext RenderOpen()
+        {
+            return _drawingVisual.RenderOpen();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            drawingContext.DrawDrawing(_drawingVisual.Drawing);
+        }
+    }
+}
---
+++ dump/TestDump/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDump
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 创建bug让程序闪退
+            //double[] doubles = new double[10];
+            //for(int i = 0; i < 12; i++) 
+            //{
+            //    doubles[i] = 5;
+            //}
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread thread = new Thread(TestDump);
+            //thread.Start();
+
+            while (true)
+            {
+                Task.Run(() =>
+                {
+                    if(Thread.CurrentThread.ManagedThreadId.ToString().Equals("1"))
+                    {
+                        textBox1.Text = "1";
+                        TextBox text = new TextBox();
+                        this.Controls.Add(text);
+                    }
+                });
+                Thread.Sleep(100);
+            }
+
+            Task.Run(() =>
+            {
+                MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
+                textBox1.Text = "1";
+                TextBox text = new TextBox();
+                this.Controls.Add(text);
+            });
+        }
+
+        private void TestDump()
+        {
+            textBox1.Text = "1";
+            TextBox text = new TextBox();
+            this.Controls.Add(text);
+        }
+    }
+}

b801914db60bb9b9f1a978b9a1cca57bbe715147
jyj <2534803898@qq.com>
2024-05-20 22:24:03 +0800

    .

--- dump/TestDump/Form1.cs
+++ dump/TestDump/Form1.cs
@@ -20,42 +20,42 @@ namespace TestDump
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // 创建bug让程序闪退
+            //创建bug让程序闪退
             //double[] doubles = new double[10];
-            //for(int i = 0; i < 12; i++) 
+            //for (int i = 0; i < 12; i++)
             //{
             //    doubles[i] = 5;
             //}
 
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Thread thread = new Thread(TestDump);
-            //thread.Start();
+            Thread thread = new Thread(TestDump);
+            thread.Start();
 
-            while (true)
-            {
-                Task.Run(() =>
-                {
-                    if(Thread.CurrentThread.ManagedThreadId.ToString().Equals("1"))
-                    {
-                        textBox1.Text = "1";
-                        TextBox text = new TextBox();
-                        this.Controls.Add(text);
-                    }
-                });
-                Thread.Sleep(100);
-            }
+            //while (true)
+            //{
+            //    Task.Run(() =>
+            //    {
+            //        if(Thread.CurrentThread.ManagedThreadId.ToString().Equals("1"))
+            //        {
+            //            textBox1.Text = "1";
+            //            TextBox text = new TextBox();
+            //            this.Controls.Add(text);
+            //        }
+            //    });
+            //    Thread.Sleep(100);
+            //}
 
-            Task.Run(() =>
-            {
-                MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
-                textBox1.Text = "1";
-                TextBox text = new TextBox();
-                this.Controls.Add(text);
-            });
+            //Task.Run(() =>
+            //{
+            //    MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
+            //    textBox1.Text = "1";
+            //    TextBox text = new TextBox();
+            //    this.Controls.Add(text);
+            //});
         }
 
         private void TestDump()

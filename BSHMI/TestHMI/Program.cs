34827656983b13156f2185f9e28e99fe70cc0b63
JYJ <2534803898@qq.com>
2025-03-19 11:28:50 +0800

    Init commit bs hmi

---
+++ BSHMI/TestHMI/Program.cs
@@ -0,0 +1,47 @@
+namespace TestHMI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            // Ê¹ÓÃ CORS ²ßÂÔ
+            app.UseCors("AllowAllOrigins");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

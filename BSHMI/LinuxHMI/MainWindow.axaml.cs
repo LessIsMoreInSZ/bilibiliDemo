56dfa66d60b67503ffa62eb68d64ebfe7f11f3b8
JYJ <2534803898@qq.com>
2025-03-19 16:43:18 +0800

    commit test

---
+++ BSHMI/LinuxHMI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LinuxHMI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
